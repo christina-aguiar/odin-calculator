1d891f303df2a7d254b6f4d03c747d14119e4fd4
christina-aguiar <123034210+christina-aguiar@users.noreply.github.com>
2023-06-27 02:04:42 +0200

    cleaner code

--- script.js
+++ script.js
@@ -1,60 +1,45 @@
 const display = document.querySelector('.display');
-const resultDisplay = document.querySelector('.resultDisplay');
 const preview = document.querySelector('.preview');
-const button1 = document.querySelector('.numerical1');
-const button2 = document.querySelector('.numerical2');
-const button3 = document.querySelector('.numerical3');
-const button4 = document.querySelector('.numerical4');
-const button5 = document.querySelector('.numerical5');
-const button6 = document.querySelector('.numerical6');
-const button7 = document.querySelector('.numerical7');
-const button8 = document.querySelector('.numerical8');
-const button9 = document.querySelector('.numerical9');
-const button0 = document.querySelector('.numerical0');
-const buttonAdd = document.querySelector('.operatorAdd');
-const buttonMinus = document.querySelector('.operatorMinus');
-const buttonDivide = document.querySelector('.operatorDivide');
-const buttonMultiply = document.querySelector('.operatorMultiply');
+const resultDisplay = document.querySelector('.resultDisplay');
+const numericalButtons = document.querySelectorAll('.numerical');
+const operatorButtons = document.querySelectorAll('.operator');
 const buttonEqual = document.querySelector('.equal');
 const buttonClear = document.querySelector('.clear');
 
 const arrayOfNums = [];
 let operand = '';
 let sum;
-let size;
-let test;
+let arraySize;
+let booleanTest;
 display.textContent = 0;
 
-button0.addEventListener('click', function () {
-    updateDisplay('0')
+numericalButtons.forEach(Element => Element.addEventListener('click', function () {
+    updateDisplay(this.textContent)
+}));
+
+operatorButtons.forEach(Element => Element.addEventListener('click', function () {
+    if (arrayOfNums.length > 0) {
+        calculate()
+        operand = Element.textContent;
+        preview.textContent = `${arrayOfNums[0]} ${operand}`
+        resultDisplay.textContent = '';
+    }
+    else {
+        operand = Element.textContent;
+        testAndPush();
+    }
+}
+));
+
+buttonEqual.addEventListener('click', function () {
+    if (arrayOfNums.length > 0) {
+        calculate()
+    }
 });
-button1.addEventListener('click', function () {
-    updateDisplay('1')
-});
-button2.addEventListener('click', function () {
-    updateDisplay('2')
-});
-button3.addEventListener('click', function () {
-    updateDisplay('3')
-});
-button4.addEventListener('click', function () {
-    updateDisplay('4')
-});
-button5.addEventListener('click', function () {
-    updateDisplay('5')
-});
-button6.addEventListener('click', function () {
-    updateDisplay('6')
-});
-button7.addEventListener('click', function () {
-    updateDisplay('7')
-});
-button8.addEventListener('click', function () {
-    updateDisplay('8')
-});
-button9.addEventListener('click', function () {
-    updateDisplay('9')
-});
+
+buttonClear.addEventListener('click', function () {
+    reset()
+})
 
 function updateDisplay(a) {
     if (resultDisplay.textContent !== '' && operand == '') {
@@ -67,115 +52,56 @@ function updateDisplay(a) {
     display.textContent = parseInt(display.textContent.concat(a))
 };
 
-buttonAdd.addEventListener('click', function () {
-    if (arrayOfNums.length > 0) {
-        calculate()
-        operand = '+'
-        showPreview()
-    }
-    else {
-        operand = '+';
-        testAndPush()
-    }
-})
-buttonMinus.addEventListener('click', function () {
-    if (arrayOfNums.length > 0) {
-        calculate()
-        operand = '-'
-        showPreview();
-    }
-    else {
-        operand = '-';
-        testAndPush()
-    }
-})
-buttonMultiply.addEventListener('click', function () {
-    if (arrayOfNums.length > 0) {
-        calculate()
-        operand = '*'
-        showPreview();
-    }
-    else {
-        operand = '*';
-        testAndPush()
-    }
-})
-buttonDivide.addEventListener('click', function () {
-    if (arrayOfNums.length > 0) {
-        calculate()
-        operand = '/'
-        showPreview()
-    }
-    else {
-        operand = '/';
-        testAndPush()
-    }
-})
-
-function showPreview() {
-    preview.textContent = `${arrayOfNums[0]} ${operand}`
-    resultDisplay.textContent = '';
-}
-
 function testAndPush() {
-    test = isNaN(parseInt(display.textContent))
-    if (test == false) {
+    booleanTest = isNaN(parseInt(display.textContent))
+    if (booleanTest == false) {
         arrayOfNums.push(parseInt(display.textContent))
-        display.textContent = '';
+        display.textContent = '0';
         preview.textContent = `${arrayOfNums[0]} ${operand}`
     }
 }
+
 function calculate() {
-    test = isNaN(parseInt(display.textContent))
-    if (test == false) {
+    booleanTest = isNaN(parseInt(display.textContent))
+    if (booleanTest == false) {
         arrayOfNums.push(parseInt(display.textContent))
     }
 
     if (arrayOfNums.length > 0) {
         if (operand == '+') {
             sum = arrayOfNums.reduce((a, b) => a + b, 0)
-            giveResult()
         }
         else if (operand == '-') {
             sum = arrayOfNums.reduce((a, b) => a - b)
-            giveResult()
         }
         else if (operand == '*') {
             sum = arrayOfNums.reduce((a, b) => a * b)
-            giveResult()
         }
         else if (operand == '/') {
             if (arrayOfNums[1] == 0) {
-                alert(`Can't divide by 0`)
-                reset()
+                alert(`Can't divide by 0. Please, re-write your calculation`);
+                reset();
+                throw new Error(`cant divide by 0! Please, re-write your calculation`)
             }
             else {
                 sum = arrayOfNums.reduce((a, b) => a / b)
-                giveResult()
             }
         }
+        arraySize = arrayOfNums.length
+        for (i = 0; i < arraySize; i++) {
+            arrayOfNums.pop()
+        }
+        arrayOfNums.push(sum);
+        display.textContent = ''
+        resultDisplay.textContent = parseInt(sum * 100) / 100;
+        preview.textContent = ''
+        operand = ''
     }
 }
-function giveResult() {
-    size = arrayOfNums.length
-    for (i = 0; i < size; i++) {
-        arrayOfNums.pop()
-    }
-    arrayOfNums.push(sum);
-    display.textContent = ''
-    resultDisplay.textContent = parseInt(sum * 100) / 100;
-    preview.textContent = ''
-    operand = ''
-}
-buttonEqual.addEventListener('click', function () {
-    if (arrayOfNums.length > 0) {
-        calculate()
-    }
-})
 
 function reset() {
-    size = arrayOfNums.length
-    for (i = 0; i < size; i++) {
+    arraySize = arrayOfNums.length
+    for (i = 0; i < arraySize; i++) {
         arrayOfNums.pop()
     }
     operand = '';
@@ -185,6 +111,3 @@ function reset() {
     preview.textContent = ''
 }
 
-buttonClear.addEventListener('click', function () {
-    reset()
-})
